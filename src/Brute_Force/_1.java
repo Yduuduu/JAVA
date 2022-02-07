1f4ea42bb6ecc61288664b51faed307ff4ec63b6
ydduddu <hou_o@naver.com>
2022-02-07 23:39:20 +0900

    first commit

--- src/Brute_Force/_1.java
+++ src/Brute_Force/_1.java
@@ -1,5 +1,38 @@
 package Brute_Force;
 
-public class _1 {
+// 모의고사
 
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        
+        int[] person1 = {1,2,3,4,5};
+        int[] person2 = {2,1,2,3,2,4,2,5};
+        int[] person3 = {3,3,1,1,2,2,4,4,5,5};
+
+        int[] cnt = new int[3];
+        
+        for(int i=0;i<answers.length;i++){
+            if(answers[i] == person1[i%5]){cnt[0]++;}
+            if(answers[i] == person2[i%8]){cnt[1]++;}
+            if(answers[i] == person3[i%10]){cnt[2]++;}
+        }
+
+        int max = Math.max(Math.max(cnt[0],cnt[1]),cnt[2]); 
+        
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        if(max == cnt[0]){list.add(1);}
+        if(max == cnt[1]){list.add(2);}
+        if(max == cnt[2]){list.add(3);}
+        
+        int[] answer = new int[list.size()];
+        
+        for(int i=0;i<list.size();i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
 }
--- src/Sort/_3.java
+++ src/Sort/_3.java
@@ -1,5 +1,25 @@
 package Sort;
 
-public class _3 {
+// H-Index
 
+import java.util.*;
+
+class Solution {
+	public int solution(int[] citations) {
+		int answer = 0;
+
+		Arrays.sort(citations);
+
+		int h;
+		for(int i=0; i<citations.length; i++){
+
+			h = citations.length-i;
+
+			if(citations[i] >= h){
+				answer = h;
+				break;
+			}
+		}
+		return answer;
+	}
 }
