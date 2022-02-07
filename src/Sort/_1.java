39df5e4df85fef379a16debdc6dcb1c3fd3f25a5
ydduddu <hou_o@naver.com>
2022-02-07 23:03:51 +0900

    first commit

--- src/Sort/_1.java
+++ src/Sort/_1.java
@@ -1,5 +1,28 @@
 package Sort;
 
-public class _1 {
+// K¹øÂ°¼ö
 
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+	int[] answer = new int[commands.length];
+		
+	for(int i = 0; i < commands.length; i++) {
+			
+		int start = commands[i][0];
+		int end = commands[i][1];
+		int select = commands[i][2];
+		int[] splitArray = new int[end - start + 1];
+		int index = 0;
+		for(int j = start-1; j<end; j++) {
+			splitArray[index] = array[j];
+			index++;
+		}
+			
+		Arrays.sort(splitArray);
+		answer[i] = splitArray[select-1];
+	}
+	return answer;			
+    }
 }
--- src/Sort/_2.java
+++ src/Sort/_2.java
@@ -1,5 +1,7 @@
 package Sort;
 
+// 가장 큰 수
+
 public class _2 {
 
 }
